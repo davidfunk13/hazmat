4001193f5a32317b6417e2926ede4cf8321460dd
Dave Funk <davefunk135@gmail.com>
2018-09-10 22:35:44 -0700

    css sorted and z-index issues sorted for hamburger menu. solution was to add another layer for opacity fixed 100% and toggle hide show onclick, and tie the animations to it.

--- public/js/hamburgerMenu.js
+++ public/js/hamburgerMenu.js
@@ -1,11 +1,13 @@
 function openNav() {
     document.getElementById("hamburger").style.width = "250px";
-    document.getElementById("container").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    // document.getElementById("container").style.marginLeft = "250px";
+    document.body.style.marginLeft = "250px";
+    document.getElementById("opacity").style.display = "block";
 }
 
 function closeNav() {
     document.getElementById("hamburger").style.width = "0";
-    document.getElementById("container").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
+    // document.getElementById("container").style.marginLeft = "0";
+    document.body.style.marginLeft = "0";
+    document.getElementById("opacity").style.display = "none";
 }
