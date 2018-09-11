c99dfb46109e99939124d7425d9fdd42b6a69d87
Dave Funk <davefunk135@gmail.com>
2018-09-10 23:20:39 -0700

    changes

--- public/js/hamburgerMenu.js
+++ public/js/hamburgerMenu.js
@@ -3,6 +3,7 @@ function openNav() {
     // document.getElementById("container").style.marginLeft = "250px";
     document.body.style.marginLeft = "250px";
     document.getElementById("opacity").style.display = "block";
+    document.getElementById('nav-logo').setAttribute('src', '/img/fixed-logo.png');
 }
 
 function closeNav() {
@@ -10,4 +11,21 @@ function closeNav() {
     // document.getElementById("container").style.marginLeft = "0";
     document.body.style.marginLeft = "0";
     document.getElementById("opacity").style.display = "none";
+    document.getElementById('nav-logo').setAttribute('src', '/img/nav-logo.png');
+}
+
+let toggle = false;
+
+function toggleNav() {
+    console.log(toggle)
+    switch (toggle) {
+        case true:
+        toggle = false;
+            return closeNav()
+        case false:
+        toggle = true;
+            return openNav()
+        default:
+            return "something went wrong"
+    }
 }
