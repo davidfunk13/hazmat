b9c8bb21d4d7d6d064c5755cbe4b71de6ae15019
Dave Funk <davefunk135@gmail.com>
2018-09-25 00:16:22 -0700

    major code refactor. seperated changes with js into orientation function that just gets caled when i need it to check things. lots or responsive fixes and overall bug fixes. looks great on most mobile phones and devices now.

--- public/js/hamburgerMenu.js
+++ public/js/hamburgerMenu.js
@@ -1,67 +1,49 @@
 let toggle = false;
 let currentWidth = window.screen.availWidth;
-// function preventBehavior(e) {
-//   console.log(e)
-//   e.preventDefault(); 
-// };
-
-// document.getElementById('video-tint').addEventListener("touchmove", preventBehavior, {passive: false});
-
 //onclick fires toggle nav
 $('#closeNav').on('click', () => {
   return toggleNav();
 });
 //if orientation changes, this function will fire changing currentWidth in global scope
-$(window).resize(function () {
-
+// window.addEventListener("orientationchange", function() {
+//   console.log('hit')
+//     console.log(`hit. lastClick: ${lastClicked}`);
+//     if (lastClicked === null) {
+//       return
+//     }
+//           $('html, body').animate({
+//             scrollTop: $(lastClicked).offset().top
+//           }, 500)
+// });
+$(window).resize( function (e) {
   currentWidth = window.screen.availWidth;
-  console.log(lastClicked)
+  console.log(`angle ${screen.orientation.angle}`)
+  console.log(`last clicked : ${lastClicked}`)
   if (lastClicked === null) {
-    console.log(`Widow Resized or Re-oriented. lastClicked is currently ${lastClicked}`)
+    console.log('is null')
+    // console.log($(lastClicked).offset().top)
   }
-  else {
-    console.log(`hit. lastClick: ${lastClicked}`);
+  else{
+    console.log('is not null')
+    console.log(lastClicked)
     $('html, body').animate({
       scrollTop: $(lastClicked).offset().top
+
     }, 0)
   }
+  orientationHandler()
+
+
+  // if (toggle === true) {
+
+  // }
 });
 
-$(window).on('orientationchange', (e) => {
-  if (toggle === true && e.target.innerWidth === 360) {
-    // document.getElementById("pushNav__container").style.transform = "translateX(23%)";
-    $('#pushNav__container').css({
-      'transition': 'none',
-      'transform': 'translate(23%)'
-    })
-  }
-  if (toggle === true && e.target.innerWidth === 640) {
-    $('#pushNav__container').css({
-      'transition': '.5s',
-      'transform': 'translateX(41%)'
-    })
-    // document.getElementById("pushNav__container").style.transform = "translateX(41%)";
-  }
-});
-
+//open and close nav
 function openNav() {
-  if ($('#pushNav__container').css('transition') === 'none 0s ease 0s') {
-    $('#pushNav__container').css({
-      'transition': 'all .5s ease 0s'
-    })
-  }
-  toggle = true;
-  document.getElementById("pushNav").style.width = "150px";
+  // toggle = true;
   //this pushes sections and text
-  console.log(currentWidth)
-  if (currentWidth === 360) {
-    document.getElementById("pushNav__container").style.transform = "translateX(41%)";
-  };
-  if (currentWidth === 640) {
-    document.getElementById("pushNav__container").style.transform = "translateX(23%)";
-  };
-  //this pushes fixed video along with the rest of the document.
-  document.getElementById("video").style.marginLeft = "150px";
+  orientationHandler()
   //this is the opacity layer. needs to be on top z-index.
   document.getElementById('opacity').style.display = "block";
   //for the logo
@@ -69,18 +51,54 @@ function openNav() {
 }
 
 function closeNav() {
-  toggle = false;
-  document.getElementById("pushNav").style.width = "0";
-  //this pushes sections and text
-  document.getElementById("pushNav__container").style.transform = "translateX(0%)";
-  //this pushes fixed video along with the rest of the document
-  document.getElementById("video").style.marginLeft = "0";
+  // toggle = false;
+  // document.getElementById("pushNav").style.width = "0";
+  // //this pushes sections and text
+  // document.getElementById("pushNav__container").style.transform = "translateX(0%)";
+  // //this pushes fixed video along with the rest of the document
+  // document.getElementById("video").style.marginLeft = "0";
+  orientationHandler()
   //this is the opacity layer. needs to be on top z-index.
   document.getElementById('opacity').style.display = "none";
   //opacity layer. top z index
   document.getElementById('menu-toggle').style.display = "block";
+  // if (lastClicked === null) {
+
+  // }
+  // else{
+  //       $('html, body').animate({
+  //     scrollTop: $(lastClicked).offset().top
+  //   }, 500)
+  // }
 }
 
+//resize listener functions
+const orientationHandler = () => {
+  // if closed, open
+  if (toggle === true) {
+
+    if (screen.orientation.angle === 0) {
+      document.getElementById("pushNav").style.width = "45%";
+
+      document.getElementById("pushNav__container").style.transform = "translateX(44%)";
+      //this pushes fixed video along with the rest of the document.
+      document.getElementById("video").style.marginLeft = "45%";
+    }
+    if (screen.orientation.angle === 90) {
+      document.getElementById("pushNav").style.width = "30%";
+      document.getElementById("pushNav__container").style.transform = "translateX(30%)";
+      //this pushes fixed video along with the rest of the document.
+      document.getElementById("video").style.marginLeft = "30%";
+    }
+  }
+  // if open, close
+  if (toggle === false) {
+    document.getElementById("pushNav").style.width = "0%";
+    document.getElementById("pushNav__container").style.transform = "translateX(0%)";
+    //this pushes fixed video along with the rest of the document.
+    document.getElementById("video").style.marginLeft = "0%";
+  }
+}
 function toggleNav() {
   switch (toggle) {
     case true:
