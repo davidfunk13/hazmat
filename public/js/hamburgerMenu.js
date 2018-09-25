af5e477dc6bd4d78f9b549c7fb5df565a4eec6c2
davidfunk13 <davefunk135@gmail.com>
2018-09-25 01:07:15 -0700

    got it. 2 events, one resize, one orientation change

--- public/js/hamburgerMenu.js
+++ public/js/hamburgerMenu.js
@@ -6,17 +6,14 @@ $('#closeNav').on('click', () => {
 });
 //if orientation changes, this function will fire changing currentWidth in global scope
 window.addEventListener("orientationchange", function() {
-console.log('hit')
+          console.log(`angle ${screen.orientation.angle}`)
+          console.log(`last clicked : ${lastClicked}`)
           orientationHandler()
-
 });
 $(window).resize( function (e) {
-  // currentWidth = window.screen.availWidth;
-  console.log(`angle ${screen.orientation.angle}`)
-  console.log(`last clicked : ${lastClicked}`)
   if (lastClicked === null) {
     console.log('is null')
-    // console.log($(lastClicked).offset().top)
+   return
   }
   else{
     console.log('is not null')
@@ -26,17 +23,11 @@ $(window).resize( function (e) {
 
     }, 0)
   }
-  // orientationHandler()
 
-
-  // if (toggle === true) {
-
-  // }
 });
 
 //open and close nav
 function openNav() {
-  // toggle = true;
   //this pushes sections and text
   orientationHandler()
   //this is the opacity layer. needs to be on top z-index.
@@ -46,40 +37,36 @@ function openNav() {
 }
 
 function closeNav() {
-  // toggle = false;
-  // document.getElementById("pushNav").style.width = "0";
-  // //this pushes sections and text
-  // document.getElementById("pushNav__container").style.transform = "translateX(0%)";
-  // //this pushes fixed video along with the rest of the document
-  // document.getElementById("video").style.marginLeft = "0";
   orientationHandler()
   //this is the opacity layer. needs to be on top z-index.
   document.getElementById('opacity').style.display = "none";
   //opacity layer. top z index
   document.getElementById('menu-toggle').style.display = "block";
-  // if (lastClicked === null) {
-
-  // }
-  // else{
-  //       $('html, body').animate({
-  //     scrollTop: $(lastClicked).offset().top
-  //   }, 500)
-  // }
 }
 
 //resize listener functions
-const orientationHandler = () => {
+function orientationHandler() {
   // if closed, open
+  console.log(screen.orientation.angle)
   if (toggle === true) {
 
     if (screen.orientation.angle === 0) {
+      console.log('vertical')
       document.getElementById("pushNav").style.width = "45%";
 
       document.getElementById("pushNav__container").style.transform = "translateX(44%)";
       //this pushes fixed video along with the rest of the document.
       document.getElementById("video").style.marginLeft = "45%";
     }
-    if (screen.orientation.angle === 90) {
+    if (screen.orientation.angle === 90 ) {
+      console.log('horizontal')
+      document.getElementById("pushNav").style.width = "30%";
+      document.getElementById("pushNav__container").style.transform = "translateX(29%)";
+      //this pushes fixed video along with the rest of the document.
+      document.getElementById("video").style.marginLeft = "30%";
+    }
+    if (screen.orientation.angle === 270 ) {
+      console.log('horizontal')
       document.getElementById("pushNav").style.width = "30%";
       document.getElementById("pushNav__container").style.transform = "translateX(30%)";
       //this pushes fixed video along with the rest of the document.
