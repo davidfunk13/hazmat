724d20d9d7194e268066296fa42e2ee24d22b33d
davidfunk13 <davefunk135@gmail.com>
2018-09-28 15:52:50 -0700

    mobile and desktop navigation switch is now complete and seemless. all browser, user interaction, and other edge cases tested and fixed. lots of work and improvements. merge to master?

--- public/js/hamburgerMenu.js
+++ public/js/hamburgerMenu.js
@@ -1,38 +1,52 @@
 let toggle = false;
-let currentWidth = window.screen.availWidth;
+// let currentWidth = window.screen.availWidth;
 //onclick fires toggle nav
-$('#menu-toggle').on('click', () => {
+$('#menu-toggle').on('click', (e) => {
+  e.preventDefault()
   return toggleNav()
 })
 
-$('#closeNav').on('click', () => {
+$('#closeNav').on('click', (e) => {
+  e.preventDefault()
   return toggleNav();
 });
-if ($(window).width() >= 1024) {
-  desktop()
-}
-if ($(window).width() <= 1024) {
-  mobile()
-}
+
 //if orientation changes, orientation function fires.
 $(window).resize(() => {
-  console.log($(window).width());
-  console.log($(window).height());
- 
- 
+  //run orientation handle function first, as to not obscure results from media queries
   orientationHandler()
+  //screen size 
+  if ($(window).width() >= 1024) {
+    console.log('desktop')
+    toggle = false;
+    desktop()
+  }
+  if ($(window).width() <= 1023) {
+    console.log('mobile')
+    mobile()
+  }
+
 });
 
 function desktop() {
-  console.log('hello')
-  closeNav();
+  closeNav()
   document.getElementById('menu-toggle').style.display = "none";
-}
-function mobile(){
-  
-    document.getElementById('menu-toggle').style.display = "block";
+ 
+
+  document.querySelector('.desktop-nav').style.display = "flex";
     document.getElementById('opacity').style.display = "none";
-    document.getElementById("pushNav__container").style.transform = "translateX(0%)";
+  // document.getElementById("pushNav").style.width = "0%";
+  // document.getElementById("pushNav__container").style.transform = "translateX(0%)";
+  // document.getElementById("video").style.marginLeft = "0%";
+// toggle = false
+}
+
+function mobile() {
+  document.querySelector('.desktop-nav').style.display = "none";
+  document.getElementById('menu-toggle').style.display = "block";
+  // document.getElementById('opacity').style.display = "none";
+  // document.getElementById("pushNav__container").style.transform = "translateX(0%)";
+  // closeNav()
 }
 //open and close nav
 function openNav() {
@@ -46,29 +60,35 @@ function openNav() {
 
 function closeNav() {
   // console.log($(lastClicked).offset().top)
+  // console.log('closing')
   orientationHandler()
   //this is the opacity layer. needs to be on top z-index.
   document.getElementById('opacity').style.display = "none";
+
   //opacity layer. top z index
   document.getElementById('menu-toggle').style.display = "block";
 }
 
 //resize listener functions
 function orientationHandler() {
+  screenOrientation = ($(window).width() > $(window).height()) ? 90 : 0;
+  console.log(screenOrientation)
   // TRUE means menu is currently OPEN
   if (toggle === true) {
+    // console.log('hit')
     if (lastClicked !== null) {
       $('html, body').animate({
         scrollTop: $(lastClicked).offset().top
       }, 0)
     }
-    if (Math.abs(window.orientation) === 0) {
+    console.log(window.orientation)
+    if (screenOrientation === 0) {
       //PORTRAIT MODE pushes elements that need to be pushed and exposes nav
       document.getElementById("pushNav").style.width = "45%";
       document.getElementById("pushNav__container").style.transform = "translateX(45%)";
       document.getElementById("video").style.marginLeft = "45%";
     }
-    if (Math.abs(window.orientation) === 90) {
+    if (screenOrientation === 90) {
       //LANDSCAPE MODE pushes elements that need to be pushed and exposes nav
       document.getElementById("pushNav").style.width = "25%";
       document.getElementById("pushNav__container").style.transform = "translateX(25%)";
@@ -77,6 +97,7 @@ function orientationHandler() {
   }
   // FALSE means menu is currently closed
   if (toggle === false) {
+    // console.log('hit')
     // if (lastClicked !== null) {
     //   $('html, body').animate({
     //     scrollTop: $(lastClicked).offset().top
--- public/js/smoothScroll.js
+++ public/js/smoothScroll.js
@@ -26,8 +26,14 @@ let lastClicked = null;
 ids.map(id => {
   $(id.link).click(() => {
     lastClicked = id.div;
-    $('html, body').animate({
-      scrollTop: $(id.div).offset().top
-    }, 1000)
+    if ($('.desktop-nav').is(":visible")) {
+      $('html, body').animate({
+        scrollTop: $(id.div).offset().top - 40
+      }, 1000)
+    } else {
+      $('html, body').animate({
+        scrollTop: $(id.div).offset().top
+      }, 1000)
+    }
   });
 })
